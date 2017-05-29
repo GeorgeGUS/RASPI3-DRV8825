b86064400006356e64980769c81d5bb266806108
Daniel Nikulin <daniel@nikul.in>
2017-05-29 18:53:05 +0200

    Improved header tags and import order
    See https://stackoverflow.com/questions/1523427/what-is-the-common-header-format-of-python-files

--- pwm.py
+++ pwm.py
@@ -2,15 +2,16 @@
 # encoding: utf-8
 
 """pwm.py: Script for controlling DRV8825 StepperMotorDrivers"""
-__author__ = "Maurice Seifert, Daniel Nikulin"
+__author__ = "Maurice Seifert"
+__credits__ = "Daniel Nikulin"
 __license__ = "MIT"
 __version__ = "1.0.0"
 
 # Import
+import sys
+import time as t
 import RPi.GPIO as GPIO
 import pigpio
-import time as t
-import sys
 
 # PINS
 dirpin = 16
